a4427f4b0128af9f8e54cae8f13811c085ef8b8e
Illinariss <42944888+Illinariss@users.noreply.github.com>
2025-06-26 14:01:05 +0200

    Add location discovery tracking

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -24,6 +24,7 @@ public partial class Game : Control
         generator.PlaceLocations(mapData, locations);
 
         mapRoot.Generator = generator;
+        mapRoot.Locations = locations;
 
         return mapRoot;
     }
